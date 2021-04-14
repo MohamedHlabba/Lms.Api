d860fe376f611150b7dbdbc693e7f43ea6673359
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-04-14 12:41:29 +0200

    Add project files.

---
+++ Lms.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lms.Core.Entities;
+
+namespace Lms.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses{ get; set; }
+        public DbSet<Module> Modules { get; set; }
+    }
+}
---
+++ Lms.Data/Data/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Bogus;
+using Lms.Api.Data;
+using Lms.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data
+{
+    class SeedData
+    {
+       
+            public static async Task InitAsync(IServiceProvider services)
+            {
+                using (var context = new ApplicationDbContext(services.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+                {
+                if (context.Courses.Any())
+                {
+                    return;
+                }
+
+                var fake = new Faker("sv");
+
+                    var courses = new List<Course>();
+
+                    for (int i = 0; i < 20; i++)
+                    {
+                        var courseClass = new Course
+                        {
+                            Title = fake.Company.CatchPhrase(),
+                            StartDate = DateTime.Now.AddDays(fake.Random.Int(-2, 2))
+                        };
+
+                    courses.Add(courseClass);
+
+                    }
+
+                    await context.AddRangeAsync(courses);
+                var modulers = new List<Module>();
+
+                foreach (var course in courses)
+                {
+ 
+                        if (fake.Random.Int(0, 5) == 0)
+                        {
+                            var module = new Module
+                            {
+                                Course = course,
+                                StartDate = DateTime.Now.AddDays(fake.Random.Int(-2, 2)),
+
+                            };
+                            modulers.Add(module);
+                        }
+                    }
+                await context.AddRangeAsync(modulers);
+                await context.SaveChangesAsync();
+
+                }
+            }
+        }
+    }
+
